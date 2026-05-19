09b65c3668de49853824163596476955ab3a207d
bleu-harsha <ministermonkeya1@gmail.com>
2026-05-19 13:41:07 +0530

    initial bland commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+*        .__                   __      _____       
+*   ____ |  |__   ____   ____ |  | ___/ ____\______
+* _/ ___\|  |  \ /  _ \ /    \|  |/ /\   __\/  ___/
+* \  \___|   Y  (  <_> )   |  \    <  |  |  \___ \ 
+*  \___  >___|  /\____/|___|  /__|_ \ |__| /____  >
+*      \/     \/            \/     \/           \/ 
+*
+* compile : gcc  
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+
+int main(int argc, char **argv){
+    //cli parser btw
+    // add null bytes,random data, said data,said data
+    // --null
+    // --random
+    // --data "data to add"
+    // size to reach
+    
+}
